d60ee03ec73f002b3b7ab9aeee4eb8cdf2d95733
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-20 21:12:01 +0700

    task33

--- task33/Program.cs
+++ task33/Program.cs
@@ -5,3 +5,29 @@
 
 // Массивы к задачам 32 и 33 можно сделать рандомными. В задаче 33 на экран выводится 
 // ТОЛЬКО одно сообщение: либо "да", либо "нет".
+
+Console.WriteLine("Введите количество элеметов массива");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для поиска его в массиве");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [A];
+Random rand = new Random();
+bool res = false;
+
+for (int i = 0; i< array.Length; i++)
+{
+    array[i] = rand.Next(-9,10);
+    if (array[i]==number)
+    {
+        res=true;
+    }
+}
+Console.WriteLine(string.Join(" , ",array));
+if (res==true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
