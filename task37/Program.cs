b3661a5ac64709a824a87f213b4a2c6f241bd5c3
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-22 12:06:56 +0700

    task37

--- task37/Program.cs
+++ task37/Program.cs
@@ -3,3 +3,16 @@
 // Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
+
+
+int [] array = {1,2,3,4,5};
+int [] resArray = new int[(array.Length + 1)/2];
+for (int i=0;i < resArray.Length;i++)
+{
+    resArray[i] = array[i] * array[array.Length - 1 - i];
+}
+if (array.Length%2==1)
+{
+    resArray[resArray.Length-1]=array[array.Length/2];
+}
+Console.WriteLine(string.Join(" , ", resArray));
